aa7a62e4c98253c64014dfcd59d82a62ecdc90b9
Raphael <raphael@seebachers.ch>
2013-07-23 13:20:06 +0200

    Minor code style improvements.

--- pem2jks.sh
+++ pem2jks.sh
@@ -7,9 +7,8 @@ other_cert=other_certificate.pem
 openssl rsa -in $key -des3 -out tmp_key.pem
 openssl pkcs12 -export -in $cert -inkey tmp_key.pem -out keystore.p12
 
-keytool -importkeystore -srckeystore keystore.p12 -srcstoretype pkcs12 -destkeystore keystore.jks -deststoretype JKS
+keytool -importkeystore -srckeystore keystore.p12 -srcstoretype pkcs12 -destkeystore keystore.jks -deststoretype JKS 
 
 keytool -import -alias other_certificate -keystore keystore.jks -file $other_cert
 
-rm tmp_key.pem
-rm keystore.p12
+rm tmp_key.pem keystore.p12
